497ebe41fd66e43774aa3f8085d372a532f5a903
Yuki Hamada <mail@yukihamada.jp>
2024-06-25 18:06:12 +0900

    v0.7

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,8 +30,21 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html lang="ja">
+        <Head>
+          <meta property="og:title" content="レシートモンスター - 経理をスマートに" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://doceater.io" />
+          <meta property="og:image" content="https://doceater.io/og-image.webp" />
+          <meta property="og:description" content="レシートスキャンで経理をスマートに。��拠保管と会計処理を一度に実現。「レシートモンスター」で、ビジネスの効率化と透明性向上を。" />
+          <meta property="og:site_name" content="レシートモンスター" />
+          <meta property="og:locale" content="ja_JP" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@doceater" />
+          <meta name="twitter:title" content="レシートモンスター - 経理をスマートに" />
+          <meta name="twitter:description" content="レシートスキャンで経理をスマートに。証拠保管と会計処理を一度に実現。「レシートモンスター」で、ビジネスの効率化と透明性向上を。" />
+          <meta name="twitter:image" content="https://doceater.io/og-image.webp" />
+        </Head>
         <body>
           <Main />
           <NextScript />
