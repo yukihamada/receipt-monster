b0cba7c429486b4a7a8f7da934b6adc2af6effa1
Yuki Hamada <mail@yukihamada.jp>
2024-06-30 14:36:44 +0900

    コンバージョンタグ追加

--- pages/success.tsx
+++ pages/success.tsx
@@ -4,10 +4,39 @@ import Head from 'next/head';
 import { Footer, Header } from '../components/CommonComponents';
 
 const Success: React.FC = () => {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV !== 'development' && typeof window.gtagEvent === 'function') {
+      window.gtagEvent('purchase', 'ecommerce', 'success', 1280);
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <Head>
         <title>レシートモンスター - 決済成功</title>
+        {process.env.NODE_ENV !== 'development' && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}`}></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}');
+                  // イベントトラッキング用の関数
+                  window.gtagEvent = function(action, category, label, value) {
+                    gtag('event', action, {
+                      event_category: category,
+                      event_label: label,
+                      value: value
+                    });
+                  }
+                `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <div className="min-h-screen flex flex-col bg-gradient-to-br from-green-100 to-green-200 text-gray-900">
         <main className="flex-grow flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
